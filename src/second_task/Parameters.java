b1709be869ccc8b58e175c4635c8fd4aa921c0ab
anfalqa <anfalaldraidi2001@gmail.com>
2023-12-04 18:21:33 +0300

    push the final editing

--- src/second_task/Parameters.java
+++ src/second_task/Parameters.java
@@ -3,20 +3,27 @@ package second_task;
 import java.util.Random;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.asserts.SoftAssert;
 
 public class Parameters {
 	
 	static WebDriver driver = new ChromeDriver();
+	static SoftAssert softassert= new SoftAssert();
 	static Random random = new Random();
 	static String URLink = "https://magento.softwaretestingboard.com/";
+	static String URLSignup="https://magento.softwaretestingboard.com/customer/account/create/"; 
+	static String URLLogin="https://magento.softwaretestingboard.com/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS8%2C/";
+	static String URLLogout="https://magento.softwaretestingboard.com/customer/account/logout/";
 	static String[] FirstName = { "Lina", "Lenda", "Aya", "Huda" };
 	static String[] LastName = { "Mohammad", "Ali", "Abdallah", "Khaled" };
 	static int randomNumber = random.nextInt((FirstName.length-1) - 0 + 1);
 	static String FName = FirstName[randomNumber];
-	static String LName = FirstName[randomNumber];
+	static String LName = LastName[randomNumber];
 	static int numberOfEmail = random.nextInt(120);
 	static String Email = FName + LName + numberOfEmail + "@Gmail.com";
-	static String Password = "Abc123456789";
-	
+	static String Password = "ABsabS123!";
+	static String MsgPath= "(//div[@class='panel header'])[1]";
+	static String MyAccount = "(//a[normalize-space()='My Account'])[1]";
+	static String welcome="(//div[@class='panel header'])";
 
 }//class end
--- src/second_task/signupAndLogin.java
+++ src/second_task/signupAndLogin.java
@@ -8,10 +8,10 @@ public class signupAndLogin extends Parameters{
 	@BeforeTest
 	public void myBeforeTest() {
 		driver.get(URLink);	
-		driver.get("https://magento.softwaretestingboard.com/customer/account/create/");
 	}
 	@Test(priority = 1)
-	public void SignUpTest() {
+	public void SignUpTest() throws InterruptedException {
+		driver.get(URLSignup);
 		WebElement UserFirstName= driver.findElement(By.id("firstname"));
 		UserFirstName.sendKeys(FName);
 		WebElement UserLastName= driver.findElement(By.id("lastname"));
@@ -24,18 +24,48 @@ public class signupAndLogin extends Parameters{
 		UserPasswordConf.sendKeys(Password);
 		WebElement SignupButton = driver.findElement(By.className("primary"));
 		SignupButton.click();
+		Thread.sleep(2000); 
 		
+		//assert 1
+		
+		String signAssert =driver.findElement(By.xpath(MsgPath)).getText();
+		softassert.assertEquals(signAssert.contains("Welcome"), true, "Validation The Signup Process");
+		 softassert.assertAll();
+
 	}//test 1 end
-	
-	@Test(priority = 2)
-	public void LoginTest() {
+	@Test(priority = 2 )
+	public void LogoutTest() throws InterruptedException {
 		
+		driver.get(URLLogout);
+		Thread.sleep(10000); 
+		 
+		//assert 2
+		 softassert.assertEquals(driver.getCurrentUrl(), URLink ,"validation of url after the logout process");
+		 softassert.assertAll();
+	}//test 3 end 
+	
+	@Test(priority = 3)
+	public void LoginTest() throws InterruptedException {
+		driver.get(URLLogin);
 		WebElement UserEmail= driver.findElement(By.id("email"));
 		UserEmail.sendKeys(Email);
+		
 		WebElement UserPassword= driver.findElement(By.id("pass"));
 		UserPassword.sendKeys(Password);
-		WebElement SignupButton = driver.findElement(By.className("login"));
-		SignupButton.click();
 		
-}//test 2 end
+		WebElement LoginButton = driver.findElement(By.id("send2"));
+		LoginButton.click();
+		
+		Thread.sleep(3000); 
+		//assert 3 
+		
+		String welcomeAssert = driver.findElement(By.xpath(welcome)).getText();
+		softassert.assertEquals(welcomeAssert.contains("Welcome"), true,"Validation The Login Process 1");
+		softassert.assertEquals(welcomeAssert.contains(FName), true,"Validation The Login Process 2");
+
+		 softassert.assertAll();
+		 
+		
+}//test 3 end
+	
 }//class end
